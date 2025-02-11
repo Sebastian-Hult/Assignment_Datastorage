808e43f08e6f92a7aa60c7cd9ec1e7d58b0e8a0b
Sebastian-Hult <sebastian.hult@utb.ecutbildning.se>
2025-02-11 16:12:51 +0100

    Second commit: Created a BaseRepository with basic CRUD methods, to be expanded upon. Also created dedicated repositories for Project, Customer and Employee who in turn inherits from BaseRepository.

---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public abstract class BaseRepository<TEntity>(DataContext context) where TEntity : class
+{
+    protected readonly DataContext _context = context;
+    protected readonly DbSet<TEntity> _dbSet = context.Set<TEntity>();
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAsync()
+    {
+        var entities = await _dbSet.ToListAsync();
+        return entities;
+    }
+
+    public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        var entity = await _dbSet.FirstOrDefaultAsync(expression);
+        return entity;
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task RemoveAsync(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using Data.Contexts;
+using Data.Entities;
+
+namespace Data.Repositories;
+
+public class CustomerRepository(DataContext context) : BaseRepository<CustomerEntity>(context)
+{
+}
